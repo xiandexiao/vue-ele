0042da8d83a550bad9595cfa80d439bf3e5416d6
xiandexiao <647887292@qq.com>
2022-01-17 17:44:02 +0800

    完成城市查询

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -5,37 +5,192 @@
         <span class="head-logo" @click="reload">ele.me</span>
       </template>
     </head-top>
+    <nav class="city-nav">
+      <div class="city-tip">
+        <span>当前定位城市：</span>
+        <span>定位不准时，请在城市列表中选择</span>
+      </div>
+      <router-link :to="'/city/' + guessCityid" class="guess-city">
+        <span>{{ guessCity }}</span>
+        <svg class="arrow-right">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+        </svg>
+      </router-link>
+    </nav>
+    <div class="city-con">
+      <div class="city-title">热门城市</div>
+      <ul>
+        <router-link
+          tag="li"
+          v-for="item in hotcity"
+          :to="'/city/' + item.id"
+          :key="item.id"
+          :title="item.name"
+          class="ellipsis"
+        >
+          {{ item.name }}
+        </router-link>
+      </ul>
+    </div>
+    <div class="city-con">
+      <template v-for="(value, key, index) in groupCitySortByLetter">
+        <div class="city-title" :key="key">
+          {{ key }}
+          <span v-if="index === 0">（按字母排序）</span>
+        </div>
+        <ul>
+          <router-link  tag="li"
+                        v-for="item in value"
+                        :to="'/city/' + item.id"
+                        :key="item.id"
+                        :title="item.name"
+                        class="ellipsis"
+          >
+            {{ item.name }}
+          </router-link>
+        </ul>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 import headTop from "../../components/header/Head";
+import { cityGuess,hotcity,groupcity } from 'src/service/getData';
 
 export default {
   data() {
-    return {}
+    return {
+      guessCity: '',
+      guessCityid: '',
+      hotcity: [],
+      groupCity: []
+    }
   },
   mounted() {
-
+    cityGuess().then(res => {
+      this.guessCity = res.name;
+      this.guessCityid = res.id;
+    });
+    hotcity().then(res => {
+      this.hotcity = res;
+    });
+    groupcity().then(res => {
+      this.groupCity = res;
+    })
   },
   components: {
     headTop
   },
-  computed: {},
+  computed: {
+    groupCitySortByLetter() {
+      let res = {};
+      for (let i = 65; i <= 90; i++) {
+        if (this.groupCity[String.fromCharCode(i)]) {
+          res[String.fromCharCode(i)] = this.groupCity[String.fromCharCode(i)];
+        }
+      }
+      return res
+    }
+  },
+  filters: {
+    trim(value) {
+      console.log(value)
+      if (!value) return '';
+      return value.trim();
+    }
+  },
   methods: {
     //点击图标刷新页面
     reload() {
       window.location.reload();
     }
-  },
+  }
 }
 </script>
 
 <style lang="less" scoped>
 @import '../../style/mixin';
 
+.city-title {
+  color: #666;
+  font-size: .55rem;
+  border-top: 1px solid #e4e4e4;
+  border-bottom: 1px solid #e4e4e4;
+  padding: .2rem .45rem;
+}
+
 .head-logo {
   font-weight: 400;
   .sc(0.7rem, #fff);
+}
+
+// 定位
+.city-nav {
+  display: flex;
+  flex-direction: column;
+  padding-top: 1.95rem;
+  background-color: #fff;
+
+  .city-tip {
+    display: flex;
+    justify-content: space-between;
+    padding: .6rem .45rem;
+
+    span:first-of-type {
+      font-size: .55rem;
+      color: #666;
+    }
+
+    span:last-of-type {
+      font-size: .475rem;
+      color: #9f9f9f;
+      font-weight: bolder;
+    }
+  }
+
+  .guess-city {
+    display: flex;
+    justify-content: space-between;
+    padding: .6rem .45rem;
+    border-top: 1px solid #e4e4e4;
+    border-bottom: 1px solid #e4e4e4;
+    align-items: center;
+
+    span:first-of-type {
+      color: #3190e8;
+      font-size: 0.75rem;
+    }
+
+    .arrow-right {
+      width: 0.6rem;
+      height: 0.6rem;
+      fill: #999;
+    }
+  }
+}
+
+// 热门
+.city-con {
+  display: flex;
+  flex-direction: column;
+  margin-top: .5rem;
+  background-color: #fff;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    li {
+      font-size: .6rem;
+      color: #3190e8;
+      cursor: pointer;
+      width: 25%;
+      padding: .55rem 0;
+      text-align: center;
+      border-right: 1px solid #e4e4e4;
+      border-bottom: 1px solid #e4e4e4;
+    }
+  }
 }
 </style>
--- src/service/getData.js
+++ src/service/getData.js
@@ -9,3 +9,18 @@ export const getCaptCode = () => fetch('/v1/captchas', {},'POST');
 
 // 账号密码登录
 export const accountLogin = (username, password, captcha_code) => fetch('/v2/login', {username, password, captcha_code}, 'POST');
+
+// 获取首页默认地址
+export const cityGuess = () => fetch('/v1/cities', {
+  type: 'guess'
+});
+
+// 获取热门城市
+export const hotcity = () => fetch('/v1/cities', {
+  type: 'hot'
+});
+
+// 获取首页所有城市
+export const groupcity = () => fetch('/v1/cities', {
+  type: 'group'
+});
